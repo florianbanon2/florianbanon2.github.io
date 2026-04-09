d51ac3c4006fc7c70bea4fe2485f1ac59bc29ade
Florian Banon <florian.banon@u.nus.edu>
2026-04-09 12:42:51 +0800

    Use wrapper clip for precise profile photo zoom (face + shoulders)

--- header.js
+++ header.js
@@ -1,6 +1,6 @@
 document.getElementById('site-header').innerHTML = `
   <header>
-    <img src="assets/florian-banon.jpg" alt="Florian Banon Photo">
+    <div class="photo-wrapper"><img src="assets/florian-banon.jpg" alt="Florian Banon Photo"></div>
     <h1>Florian Banon</h1>
     <p>M.Eng. Aerospace & Computer Science – Machine Learning for SAR Imagery</p>
   </header>
