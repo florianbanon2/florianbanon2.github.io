4d5a384e79269962032731172e90dd2961891e24
Florian Banon <florian.banon@u.nus.edu>
2026-04-09 11:41:17 +0800

    Initial CV website

---
+++ header.js
@@ -0,0 +1,7 @@
+document.getElementById('site-header').innerHTML = `
+  <header>
+    <img src="assets/florian-banon.jpg" alt="Florian Banon Photo">
+    <h1>Florian Banon</h1>
+    <p>M.Eng. Aerospace & Computer Science – Machine Learning for SAR Imagery</p>
+  </header>
+`;
